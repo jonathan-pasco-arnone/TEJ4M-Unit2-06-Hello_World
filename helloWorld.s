0d1826da80a24fc8c51d7bfe1bb37894e0206d32
jonathan-pasco-arnone <jonathan.pasco-arnone@mths.ca>
2022-03-25 14:06:34 +0000

    initial

---
+++ helloWorld.s
@@ -0,0 +1,27 @@
+/* Created by: Jonathan Pasco-Arnone
+ * Created March 2022
+ * This program prints Hello, World!
+ */
+
+.data
+
+/* data section */
+msg:
+.asciz "Hello, Wolrd!\n"
+len = . - msg
+
+.text
+
+.global _start
+_start:
+
+  mov   %r0, $1           /* our status is stdout */
+  ldr   %r1, =msg         /* message location in memory */
+  ldr   %r2, =len         /* message length in bytes */
+  mov   %r7, $4           /* write is syscall #4 */
+  swi   $0                /* invoke syscall */
+
+/* syscall exit */
+  mov   %r0, $0           /* our status is now exit */
+  mov   %r7, $1           /* exist is syscall #1 */
+  swi   $0                /* invoke syscall */
